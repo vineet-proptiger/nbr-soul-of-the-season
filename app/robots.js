0a2339b0bbf20a9a52f8abac6426f5ccfad446aa
vineet-proptiger <vineet.mishra@proptiger.com>
2026-08-12 16:03:37 +0530

    some fixes

--- app/robots.js
+++ app/robots.js
@@ -2,8 +2,8 @@ export default function robots() {
   return {
     rules: {
       userAgent: '*',
-      allow: ['/', '/sector-46-gurgaon'],
-      disallow: ['/vin', '/sector-46-gurgaon'],
+      allow: ['/'],
+      // disallow: ['/vin', '/sector-46-gurgaon'],
     },
     sitemap: 'https://nbrsouloftheseason.co.in/sitemap.xml',
   }
